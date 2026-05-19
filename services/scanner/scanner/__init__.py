2e3e05c7efe1d06ec2953a176eb436dd3857a107
Islam Elbanna <36525300+islamelbannamcm@users.noreply.github.com>
2026-05-20 00:45:47 +0200

    feat(scanner): add package init

---
+++ services/scanner/scanner/__init__.py
@@ -0,0 +1,9 @@
+"""AzureLens scanning engine.
+
+Logical subpackages (added in later phases):
+  - scanner.azure     : Azure Resource Graph + ARM
+  - scanner.m365      : Microsoft Graph (Entra, EXO, SPO, Teams, OD)
+  - scanner.intune    : Intune devices & policies
+  - scanner.defender  : Defender XDR + Defender for Cloud
+  - scanner.purview   : Purview governance
+"""
